d79610a33ac4245ef1d5bfa8ad7fd4dc274756f2
hanosong <838519800@qq.com>
2023-02-03 20:02:08 +0800

    代码分离-自定义分包的基本配置

---
+++ webpack/07_自定义分包/build/about-chunk.js
@@ -0,0 +1,15 @@
+(self["webpackChunkbabel_core_demo"] = self["webpackChunkbabel_core_demo"] || []).push([["about"],{
+
+/***/ "./src/router/about.js":
+/*!*****************************!*\
+  !*** ./src/router/about.js ***!
+  \*****************************/
+/***/ (function() {
+
+var h1 = document.createElement("h1");
+h1.textContent = "about-page";
+document.body.append(h1);
+
+/***/ })
+
+}]);
---
+++ webpack/07_自定义分包/webpack分包处理.md
@@ -0,0 +1,5 @@
+#### 3.SplitChunks
+
+> 底层是使用 splitChunkPlugin 实现的
+> webapack 已集成，不需要单独安装
+> 只需要提供 SplitChunksPlugin 相关的配置信息即可
